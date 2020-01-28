842f270c7ea4b08d368a201bd595820bdea82797
Naveen <naveendevinda163@gmail.com>
2020-01-28 23:24:14 +0530

    Create new kotlin

---
+++ app/src/main/java/com/thenewboston/kotlin/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.thenewboston.kotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    fun onClick(view: View) {
+        /*var text = findViewById<TextView>(R.id.button)
+        text.setText("hello")*/
+        val intent = Intent(this,Main2Activity::class.java)
+        startActivity(intent)
+    }
+}
